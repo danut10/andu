f0e76b02a88987fdfb7c26b6d9af287b83a928b7
Cretu Alexandru <alexandru.cretu@cegeka.com>
2018-12-05 16:51:09 +0200

    first task 2.0

---
+++ src/main/java/com/cegeka/test/ui/Main.java
@@ -0,0 +1,26 @@
+package com.cegeka.test.ui;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @version  $Revision$, $Date$
+ */
+public class Main
+{
+	//~ Static fields/initializers ---------------
+	/**  */
+	private static final String christmassTree = ""
+		+ "  *\n"
+		+ " * *\n"
+		+ "* * *\n";
+	//~ Methods ----------------------------------
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  args
+	 */
+	public static void main(String[] args)
+	{
+		System.out.println(christmassTree);
+	}
+}
