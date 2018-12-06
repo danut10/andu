6e3c109fa22ea095c564e9e26d97749946764bb6
AlexandruBusuioc <alexandrumariusbusuioc@gmail.com>
2018-12-06 16:22:37 +0200

    show dinamic tree

--- src/main/java/com/cegeka/test/ui/busu/Main.java
+++ src/main/java/com/cegeka/test/ui/busu/Main.java
@@ -1,5 +1,8 @@
 package com.cegeka.test.ui.busu;
 
+import java.util.Scanner;
+
+
 /**
  * DOCUMENT ME!
  *
@@ -15,7 +18,17 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		showHardcodedTree();
+		// showHardcodedTree();
+		showDinamicTree();
+	}
+	
+	/** DOCUMENT ME! */
+	private static void showDinamicTree()
+	{
+		Scanner s = new Scanner(System.in);
+		System.out.println("Enter height:");
+		int height = s.nextInt();
+		showDinamicTree(height);
 	}
 	
 	/** DOCUMENT ME! */
@@ -25,5 +38,42 @@ public class Main
 		System.out.println("  ***");
 		System.out.println(" *****");
 		System.out.println("   *  ");
+	}
+	
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  height
+	 */
+	private static void showDinamicTree(int height)
+	{
+		int blancSpaces = height - 1;
+		for (int i = 0; i < height; i++)
+		{
+			boolean addedBlancSpaces = false;
+			if (!addedBlancSpaces)
+			{
+				for (int j = 0; j < blancSpaces; j++)
+				{
+					System.out.print(" ");
+				}
+				addedBlancSpaces = true;
+			}
+			blancSpaces--;
+			for (int k = 0; k < ((i * 2) + 1); k++)
+			{
+				System.out.print("*");
+			}
+			System.out.println();
+		}
+		showTrunk(height);
+	}
+
+	private static void showTrunk(int height) {
+		for (int i = 0; i < (height - 1); i++)
+		{
+			System.out.print(" ");
+		}
+		System.out.println("*");
 	}
 }
