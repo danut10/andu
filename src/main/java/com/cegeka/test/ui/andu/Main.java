00a6d5fa9b92ca349e68782f6136105c790001e4
Cretu Alexandru <alexandru.cretu@cegeka.com>
2018-12-06 14:14:46 +0200

    Killing jalopy

--- src/main/java/com/cegeka/test/ui/andu/Main.java
+++ src/main/java/com/cegeka/test/ui/andu/Main.java
@@ -3,55 +3,30 @@ package com.cegeka.test.ui.andu;
 import java.util.Scanner;
 
 
-/**
- * DOCUMENT ME!
- *
- * @version  $Revision$, $Date$
- */
-public class Main
-{
-	//~ Static fields/initializers ---------------
-	/**  */
+public class Main {
 	private static final String TREE_PART = "*";
-
-	/**  */
 	private static final String EMPTY_SPACE = " ";
-	//~ Instance fields --------------------------
-	/**  */
-	private int height;
-	//~ Methods ----------------------------------
-	/**
-	 * DOCUMENT ME!
-	 *
-	 * @param   height
-	 * @return
-	 */
-	private String getTree(int height)
-	{
+	
+	private String getTree(int height) {
 		StringBuilder builder = new StringBuilder();
-		if (height < 1)
-		{
+		if (height < 1) {
 			System.out.println("Please enter a height > 0");
 
-			return EMPTY_SPACE;
+			return null;
 		}
-		for (int i = 1; i <= height; i++)
-		{
+		for (int i = 1; i <= height; i++) {
 			// empty spaces height + i
 			// stars height + empty spaces
-			for (int empty = 1; empty <= (height - i); empty++)
-			{
+			for (int empty = 1; empty <= (height - i); empty++) {
 				builder.append(EMPTY_SPACE);
 			}
-			for (int star = 1; star <= i; star++)
-			{
+			for (int star = 1; star <= i; star++) {
 				builder.append(TREE_PART);
 				builder.append(EMPTY_SPACE);
 			}
 			builder.append("\n");
 		}
-		for (int x = 1; x < height; x++)
-		{
+		for (int x = 1; x < height; x++) {
 			builder.append(EMPTY_SPACE);
 		}
 		builder.append(TREE_PART);
@@ -59,14 +34,8 @@ public class Main
 		return builder.toString();
 	}
 	
-	/**
-	 * DOCUMENT ME!
-	 *
-	 * @param  args
-	 */
-	public static void main(String[] args)
-	{
-		System.out.print("Please enter a height for the tree:");
+	public static void main(String[] args) {
+		System.out.print("Please enter a height for the tree: ");
 		Scanner in = new Scanner(System.in);
 		int height = in.nextInt();
 		System.out.println(new Main().getTree(height));
