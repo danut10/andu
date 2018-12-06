ef87085a944cd32a8b5bf073a4d843185e254be7
AlexandruBusuioc <alexandrumariusbusuioc@gmail.com>
2018-12-06 14:26:26 +0200

    show harcoded tree

---
+++ src/main/java/com/cegeka/test/ui/busu/Main.java
@@ -0,0 +1,33 @@
+package com.cegeka.test.ui.busu;
+
+/**
+ * DOCUMENT ME!
+ *
+ * @version  $Revision$, $Date$
+ */
+public class Main
+{
+	//~ Methods ----------------------------------
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param  args
+	 */
+	public static void main(String[] args)
+	{
+		showHardcodedTree();
+	}
+	
+	/** DOCUMENT ME! */
+	private static void showHardcodedTree()
+	{
+		System.out.println("      *");
+		System.out.println("     * *");
+		System.out.println("    * * *");
+		System.out.println("   * * * *");
+		System.out.println("  * * * * *");
+		System.out.println(" * * * * * * ");
+		System.out.println("* * * * * * *");
+		System.out.println("      *      ");
+	}
+}
