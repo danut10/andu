2b9b52f82c07402fce54e0823c6758eaa6b51a4c
Cretu Alexandru <alexandru.cretu@cegeka.com>
2018-12-06 11:01:16 +0200

    task 2 1.0

--- src/main/java/com/cegeka/test/ui/Main.java
+++ src/main/java/com/cegeka/test/ui/Main.java
@@ -1,5 +1,8 @@
 package com.cegeka.test.ui;
 
+import java.util.Scanner;
+
+
 /**
  * DOCUMENT ME!
  *
@@ -12,8 +15,58 @@ public class Main
 	private static final String christmassTree = ""
 		+ "  *\n"
 		+ " * *\n"
-		+ "* * *\n";
+		+ "* * *\n"
+		+ "  *\n";
+
+	/**  */
+	private static final String TREE_PART = "*";
+
+	/**  */
+	private static final String EMPTY_SPACE = " ";
+	//~ Instance fields --------------------------
+	/**  */
+	private int height;
 	//~ Methods ----------------------------------
+
+	/**
+	 * DOCUMENT ME!
+	 *
+	 * @param   height
+	 * @return
+	 */
+	private String getTree(int height)
+	{
+		StringBuilder builder = new StringBuilder();
+		if (height < 1)
+		{
+			System.out.println("Please enter a height > 0");
+
+			return EMPTY_SPACE;
+		}
+		for (int i = 1; i <= height; i++)
+		{
+			// empty spaces height + i
+			// stars height + empty spaces
+			for (int empty = 1; empty <= (height - i); empty++)
+			{
+				builder.append(EMPTY_SPACE);
+			}
+			for (int star = 1; star <= i; star++)
+			{
+				builder.append(TREE_PART);
+				builder.append(EMPTY_SPACE);
+			}
+			builder.append("\n");
+		}
+		for (int x = 1; x < height; x++)
+		{
+			builder.append(EMPTY_SPACE);
+		}
+		builder.append(TREE_PART);
+
+		return builder.toString();
+	}
+	
 	/**
 	 * DOCUMENT ME!
 	 *
@@ -21,6 +74,9 @@ public class Main
 	 */
 	public static void main(String[] args)
 	{
-		System.out.println(christmassTree);
+		System.out.print("Please enter a height for the tree:");
+		Scanner in = new Scanner(System.in);
+		int height = in.nextInt();
+		System.out.println(new Main().getTree(height));
 	}
 }
