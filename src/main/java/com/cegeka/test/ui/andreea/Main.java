f4f1f9aebebd4dadb1cc3aa7ba58763b6abdb9db
Radulescu Andreea <andreea.radulescu@cegeka.com>
2018-12-20 12:40:46 +0200

    final solution

--- src/main/java/com/cegeka/test/ui/andreea/Main.java
+++ src/main/java/com/cegeka/test/ui/andreea/Main.java
@@ -2,17 +2,26 @@ package com.cegeka.test.ui.andreea;
 
 public class Main {
 	
+	public static final int nbOfLayers = 3;
+	
 	public static void main(String[] args) {
-        int[][] a = buildMatrix(3, 5);
-        printMatrix(a, 3, 5);
+		
+		int lines = nbOfLayers * 3 + nbOfLayers * (nbOfLayers - 1)/2;
+		int columns = (nbOfLayers + 1) * 2 + 1;
+        int[][] a = buildMatrix(lines, columns);
+        printMatrix(a, lines, columns);
 
 	}
 	
     private static int[][] buildMatrix(int n, int m) {
         int[][] a = new int[n][m];
-        for(int i = 0; i < n; i ++ ) {
-            for(int j = 0; j < m; j++) {
-                a[i][j] = (j<(m-1)/2 - i) || (j > (m-1)/2 + i) ? 0 : 1;
+        int line = 0;
+        for (int layer = 0; layer < nbOfLayers; layer++) {
+        	for(int i = 0; i < layer + 3; i++) {
+                for(int j = 0; j < m; j++) {
+                    a[line][j] = (j < (m-1)/2 - i) || (j > (m-1)/2 + i) ? 0 : 1;
+                }
+                line++;
             }
         }
         return a;
@@ -28,3 +37,6 @@ public class Main {
     }
 
 }
+
+
+
