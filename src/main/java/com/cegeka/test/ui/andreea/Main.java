f0373830ce3ef61a00e72645c8c7ee62fc680b58
Radulescu Andreea <andreea.radulescu@cegeka.com>
2019-01-03 16:19:36 +0200

    fixed hardcoded values

--- src/main/java/com/cegeka/test/ui/andreea/Main.java
+++ src/main/java/com/cegeka/test/ui/andreea/Main.java
@@ -2,21 +2,23 @@ package com.cegeka.test.ui.andreea;
 
 public class Main {
 	
-	public static final int nbOfLayers = 3;
+	public static final int BRANCHES = 3;
+	public static final char SPACE = ' ';
+	public static final char STAR = '*';
 	
 	public static void main(String[] args) {
 		
-		int lines = nbOfLayers * 3 + nbOfLayers * (nbOfLayers - 1)/2;
-		int columns = (nbOfLayers + 1) * 2 + 1;
+		int lines = BRANCHES * 3 + BRANCHES * (BRANCHES - 1)/2;
+		int columns = (BRANCHES + 1) * 2 + 1;
         int[][] a = buildMatrix(lines, columns);
-        printMatrix(a, lines, columns);
+        printMatrix(a);
 
 	}
 	
     private static int[][] buildMatrix(int n, int m) {
         int[][] a = new int[n][m];
         int line = 0;
-        for (int layer = 0; layer < nbOfLayers; layer++) {
+        for (int layer = 0; layer < BRANCHES; layer++) {
         	for(int i = 0; i < layer + 3; i++) {
                 for(int j = 0; j < m; j++) {
                     a[line][j] = (j < (m-1)/2 - i) || (j > (m-1)/2 + i) ? 0 : 1;
@@ -27,16 +29,13 @@ public class Main {
         return a;
     }
     
-    private static void printMatrix(int[][] a, int n, int m) {
-        for(int i = 0; i < n; i ++ ) {
-            for(int j = 0; j < m; j++) {
-                System.out.print(a[i][j] == 0 ? " " : "*");
+    private static void printMatrix(int[][] a) {
+        for(int i = 0; i < a.length; i ++ ) {
+            for(int j = 0; j < a[i].length; j++) {
+                System.out.print(a[i][j] == 0 ? SPACE : STAR);
             }
             System.out.println();
         }
     }
 
 }
-
-
-
